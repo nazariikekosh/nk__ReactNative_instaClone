c5dc7efb7b81ad30e55b89dbb3c00fab31985322
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-27 14:09:53 +0300

    add images

--- instaCloneApp/src/components/screensComponents/Stories.js
+++ instaCloneApp/src/components/screensComponents/Stories.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Entypo from "react-native-vector-icons/Entypo"
 
-const stories = () => {
+
+const Stories = () => {
+  const storyInfo = [
+    {
+      id: 1,
+      name: 'Your Story',
+      Image: require('../../storage/images/userProfile.jpg'),
+    },
+  ];
+
+
+
   return (
-    <View>
-      <Text>Story</Text>
-    </View>
+    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{paddingVertical: 20}}>
+      {
+        storyInfo.map((data, index) => {
+          return(
+            <TouchableOpacity key={index}>
+              <View>
+                {
+                  data.id == 1 ?
+                  (
+                    <View>
+                      <Entypo name='circle-with-plus' style={{fontSize: 20, color:"#405de6", backgroundColor: 'white', borderRadius: 1000}}/>
+                    </View>
+                  ) :null
+                }
+              </View>
+            </TouchableOpacity>
+          )
+        })
+      }
+    </ScrollView>
   )
 }
-
-export default stories
+export default Stories
