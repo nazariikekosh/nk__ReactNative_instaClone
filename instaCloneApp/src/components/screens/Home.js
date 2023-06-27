b67a0445ff0db34046ceb0f3981036b19617d4aa
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-27 11:43:44 +0300

    add homescreen statusbar

--- instaCloneApp/src/components/screens/Home.js
+++ instaCloneApp/src/components/screens/Home.js
@@ -1,10 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'react-native'
+import FontAwesome from "react-native-vector-icons/FontAwesome"
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <StatusBar backgroundColor="white" barStyle="dark-content" animated={true} />
+      <View >
+        <FontAwesome name="plus-square-o" style={{ fontSize: 24 }} />
+        <Text style={{ fontFamily: 'Lobster-Regular', fontSize: 25, fontWeight: '500'}}>
+          Instagram
+        </Text>
+      </View>
     </View>
   )
 }
