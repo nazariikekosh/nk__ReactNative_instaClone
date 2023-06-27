3bcbc5e744a7029f339e5d08f290cdf18ca3d3bf
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-27 12:35:32 +0300

    add story

--- instaCloneApp/src/components/screens/Home.js
+++ instaCloneApp/src/components/screens/Home.js
@@ -1,18 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'react-native'
 import FontAwesome from "react-native-vector-icons/FontAwesome"
+import Feather from "react-native-vector-icons/Feather"
+import Stories from '../../components/screensComponents/Stories';
+
 
 const Home = () => {
   return (
-    <View>
+    <View style={{backgroundColor: 'white', height: '100%'}}>
       <StatusBar backgroundColor="white" barStyle="dark-content" animated={true} />
-      <View >
+      <View style={{
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingHorizontal: 15,
+        alignItems: 'center',
+      }}>
         <FontAwesome name="plus-square-o" style={{ fontSize: 24 }} />
         <Text style={{ fontFamily: 'Lobster-Regular', fontSize: 25, fontWeight: '500'}}>
           Instagram
         </Text>
+        <Feather name='navigation' style={{ fontSize: 24 }} />
       </View>
+
+      <ScrollView>
+        <Stories />
+      </ScrollView>
     </View>
   )
 }
