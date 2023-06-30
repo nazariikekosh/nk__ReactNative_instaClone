10c84325db0b3dcc05d28020b95d244ff23b9ae2
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-06-30 14:05:15 +0300

    add modal to images

--- instaCloneApp/src/components/screensComponents/SearchContent.js
+++ instaCloneApp/src/components/screensComponents/SearchContent.js
@@ -1,5 +1,30 @@
-import {View, Text, TouchableOpacity, Image} from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity, Image, Modal } from 'react-native';
+import React, { useState } from 'react';
+
+const FullScreenImage = ({ image, onClose }) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      onRequestClose={onClose}
+    >
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onPress={onClose}
+      >
+        <Image
+          source={image}
+          style={{ width: '80%', height: '80%', resizeMode: 'contain' }}
+        />
+      </TouchableOpacity>
+    </Modal>
+  );
+};
 
 const SearchContent = () => {
   const searchData = [
@@ -35,24 +60,31 @@ const SearchContent = () => {
     },
   ];
 
+  const [fullScreenImage, setFullScreenImage] = useState(null);
+
   return (
     <View>
       {searchData.map((data, index) => {
         return (
-          <>
+          <React.Fragment key={data.id}>
             {data.id === 0 ? (
               <View
                 style={{
                   flexDirection: 'row',
                   flexWrap: 'wrap',
                   justifyContent: 'space-between',
-                }}>
+                }}
+              >
                 {data.images.map((imageData, imgIndex) => {
                   return (
-                    <TouchableOpacity style={{paddingBottom: 2}}>
+                    <TouchableOpacity
+                      key={imgIndex}
+                      style={{ paddingBottom: 2 }}
+                      onPress={() => setFullScreenImage(imageData)}
+                    >
                       <Image
                         source={imageData}
-                        style={{width: 129, height: 150}}
+                        style={{ width: 129, height: 150 }}
                       />
                     </TouchableOpacity>
                   );
@@ -64,63 +96,92 @@ const SearchContent = () => {
                 style={{
                   flexDirection: 'row',
                   justifyContent: 'space-between',
-                }}>
+                }}
+              >
                 <View
                   style={{
                     flexDirection: 'row',
                     flexWrap: 'wrap',
                     width: 261,
                     justifyContent: 'space-between',
-                  }}>
+                  }}
+                >
                   {data.images.slice(0, 4).map((imageData, imgIndex) => {
                     return (
-                      <TouchableOpacity style={{paddingBottom: 2}}>
+                      <TouchableOpacity
+                        key={imgIndex}
+                        style={{ paddingBottom: 2 }}
+                        onPress={() => setFullScreenImage(imageData)}
+                      >
                         <Image
                           source={imageData}
-                          style={{width: 129, height: 150}}
+                          style={{ width: 129, height: 150 }}
                         />
                       </TouchableOpacity>
                     );
                   })}
                 </View>
-                <TouchableOpacity style={{marginLeft: 2}}>
-                    <Image source={data.images[5]} style={{width: 129, height: 300}} />
+                <TouchableOpacity
+                  style={{ marginLeft: 2 }}
+                  onPress={() => setFullScreenImage(data.images[5])}
+                >
+                  <Image
+                    source={data.images[5]}
+                    style={{ width: 129, height: 300 }}
+                  />
                 </TouchableOpacity>
               </View>
             ) : null}
-            {
-                data.id === 2 ?
-                (
-                    <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-
-                    }}>
-                        <TouchableOpacity style={{paddingRight: 2}}>
-                            <Image source={data.images[2]} style={{width: 260, height: 300}} />
-                        </TouchableOpacity>
-                        <View style={{
-                            flexDirection: 'row',
-                            flexWrap: 'wrap',
-                            width: 130,
-                            justifyContent: 'space-between',
-                        }}>
-                            {
-                                data.images.slice(0,2).map((imageData, imgIndex)=>{
-                                    return(
-                                        <TouchableOpacity style={{paddingBottom: 2}}>
-                                            <Image source={imageData} style={{width: 129, height: 150}} />
-                                        </TouchableOpacity>
-                                    )
-                                })
-                            }
-                        </View>
-                    </View>
-                ):null
-            }
-          </>
+            {data.id === 2 ? (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <TouchableOpacity
+                  style={{ paddingRight: 2 }}
+                  onPress={() => setFullScreenImage(data.images[2])}
+                >
+                  <Image
+                    source={data.images[2]}
+                    style={{ width: 260, height: 300 }}
+                  />
+                </TouchableOpacity>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    width: 130,
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  {data.images.slice(0, 2).map((imageData, imgIndex) => {
+                    return (
+                      <TouchableOpacity
+                        key={imgIndex}
+                        style={{ paddingBottom: 2 }}
+                        onPress={() => setFullScreenImage(imageData)}
+                      >
+                        <Image
+                          source={imageData}
+                          style={{ width: 129, height: 150 }}
+                        />
+                      </TouchableOpacity>
+                    );
+                  })}
+                </View>
+              </View>
+            ) : null}
+          </React.Fragment>
         );
       })}
+      {fullScreenImage && (
+        <FullScreenImage
+          image={fullScreenImage}
+          onClose={() => setFullScreenImage(null)}
+        />
+      )}
     </View>
   );
 };
